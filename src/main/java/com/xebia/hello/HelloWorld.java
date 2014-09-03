d79f174d8bba51776d5ed222e2c467c5db4d30cb
Emmanuel Servent <eservent.prestataire@vsc-technologies.com>
2014-09-03 14:57:46 +0200

    First commit Hello World App for x-delivery

---
+++ src/main/java/com/xebia/hello/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.xebia.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\">");
+        out.println("<title>Bonjour tout le monde&amp;nbsp;!</title>");
+        String env = System.getenv("env");
+        if (env == null) {
+            env = "No env defined";
+        }
+        out.println("<p>Hello world: " + env + "</p>");
+    }
+
+}
